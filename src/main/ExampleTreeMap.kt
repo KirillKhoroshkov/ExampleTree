138bd9a82d94f6a8f9369b6aee97206bcb02b3c8
kirillkhoroshkov <khoroshkov_kirill@mail.ru>
2017-12-07 14:06:37 +0300

    Доделал

--- src/main/ExampleTreeMap.kt
+++ src/main/ExampleTreeMap.kt
@@ -150,4 +150,51 @@ class ExampleTreeMap<K : Comparable<K>, V> {
             return sb.toString()
         }
     }
+
+    fun prn(){
+        var nodes = mutableListOf<Node<K, V>?>()
+        var sb = StringBuilder()
+        if (root != null) {
+            println("($root)")
+            nodes.add(root)
+            var currentList = mutableListOf<Node<K, V>?>()
+            while (!nodes.isEmpty()) {
+                var hasNotNull = false
+                while (!nodes.isEmpty()) {
+                    val current = nodes[0]
+                    nodes.removeAt(0)
+                    if (current != null) {
+                        if (current.left != null) {
+                            sb.append("(" + current.left + ", ")
+                            hasNotNull = true
+                            currentList.add(current.left)
+                        } else {
+                            sb.append("(N=N, ")
+                            currentList.add(null)
+                        }
+                        if (current.right != null) {
+                            sb.append(current.right.toString() + ")")
+                            hasNotNull = true
+                            currentList.add(current.right)
+                        } else {
+                            sb.append("N=N)")
+                            currentList.add(null)
+                        }
+                    } else {
+                        currentList.add(null)
+                        currentList.add(null)
+                        sb.append("(N=N, N=N)")
+                    }
+                }
+                if (hasNotNull) {
+                    println(sb)
+                    sb = StringBuilder()
+                    nodes = currentList
+                    currentList = mutableListOf()
+                }
+            }
+        } else {
+            println("It is empty")
+        }
+    }
 }
--- src/test/Tests.kt
+++ src/test/Tests.kt
@@ -31,4 +31,20 @@ class Tests {
         println(tree.get(9))
         println(tree.get(6))
     }
+
+    @Test fun second() {
+        val tree = ExampleTreeMap<Int, Int>()
+        tree.put(3, 3)
+        tree.put(4, 4)
+        tree.put(2, 2)
+        tree.put(1, 1)
+        tree.put(5, 5)
+        tree.put(6, 6)
+        tree.put(7, 7)
+        tree.put(8, 8)
+        tree.put(9, 9)
+        println(tree)
+        println("_________________")
+        tree.prn()
+    }
 }
